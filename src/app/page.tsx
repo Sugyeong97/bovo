c6613a2d0843db25c9569db51a839426f1797754
LSG <sugyeong9611@gmail.com>
2025-09-29 14:11:56 +0900

    msg

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import MainPage from "@/components/organism/MainPage";
+
+export default function page() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center bg-gray-300">
+      <MainPage />
+    </div>
+  );
+}
---
+++ src/components/organism/MainPage.tsx
@@ -0,0 +1,7 @@
+export default function MainPage() {
+  return (
+    <div className="w-full h-full max-w-6xl bg-white">
+      <div></div>
+    </div>
+  );
+}
