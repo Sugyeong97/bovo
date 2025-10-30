d1c170c9bf3019499d1e0c5ab3f821c463cd974a
LSG <sugyeong9611@gmail.com>
2025-10-30 09:13:34 +0900

    gallery

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,12 +1,13 @@
 import AbleList from "@/components/molecules/AbleList";
 import EnableList from "@/components/molecules/EnableList";
 import BaseLayout from "@/components/organism/BaseLayout";
+import Gallery from "@/components/organism/Gallery";
 
 export default function Page() {
     return (
         <BaseLayout>
             <section className="w-full h-full p-7 mr-24 border border-light-gray rounded-xl bg-white flex flex-col items-start z-10 shadow-inner">
-                사진첩
+                <Gallery />
             </section>
             <div className="absolute top-0 right-2">
                 <ul className="mt-5 w-24 h-1/2 flex flex-col gap-2">
---
+++ src/components/organism/Gallery.tsx
@@ -0,0 +1,10 @@
+export default function Gallery() {
+    return (
+        <section className="w-full">
+            <article className="w-full">
+                <div className="bg-gray-300 w-full h-1"></div>
+                <div>사진</div>
+            </article>
+        </section>
+    );
+}
